b3a844fbd80e865d5066c974d8c17ce58019d2c4
Gildardo Ortiz Mercado <>
2018-05-24 23:42:45 -0500

    Se modifican cambios para enviar correo

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -2,57 +2,41 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 
-// let smtpConfig = {
-//     host: 'smtp.example.com',
-//     port: 587,
-//     secure: false, // upgrade later with STARTTLS
-//     auth: {
-//         user: 'username',
-//         pass: 'password'
-//     }
-// };
-// service: 'gmail',
-// auth: {
-//   user: 'chilcho1939@gmail.com',
-//   pass: '01049119391945Gil'
-// }
-
 router.get('/saludar', (req, res) => {
-    console.log('Me la pelan turistas');
     res.status(200).json({
-      message: 'success'
+        message: 'success'
     });
 });
-
-router.get('/sendMail', (req, res) => {
+router.post('/sendMail', (req, res) => {
     var transporter = nodemailer.createTransport({
-        host: 'smtp.betostrucking.com',
-        port: 587,
-        secure: false, // upgrade later with STARTTLS
+        host: 'mail.betostrucking.com'
+        , port: 587
+        , secure: false,
         auth: {
-            user: 'ventas@betostrucking.com',
-            pass: '05*Abril/2018'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'ventas@betostrucking.com',
-        to: 'ventas@betostrucking.com',
-        subject: req.body.subject,
-        text: req.body.message
-      };
-            
-      transporter.sendMail(mailOptions, function(error, info){
+            user: 'ventas@betostrucking.com'
+            , pass: '05*Abril/2018'
+        }, tls: {rejectUnauthorized: false}
+    });
+    var mailOptions = {
+        from: 'ventas@betostrucking.com'
+        , to: 'ventas@betostrucking.com'
+        , subject: req.body.subject
+        , text: req.body.message
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-          res.status(200).json({
-            message: 'success'
-            , obj: info.response
-          });
+            console.log(error);
+            res.status(500).json({
+                message: 'Error, no se pudo enviar el correo'
+            });
         }
-      });
+        else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).json({
+                message: 'success'
+                , obj: info.response
+            });
+        }
+    });
 });
-
 module.exports = router;
