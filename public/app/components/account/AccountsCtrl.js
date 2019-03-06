f05b05e5e2516a53d9870e384f35d81de099ab47
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2019-03-05 18:14:16 -0600

    se agrega validacion para correos electronicos en el front-end

--- public/app/components/account/AccountsCtrl.js
+++ public/app/components/account/AccountsCtrl.js
@@ -18,6 +18,46 @@ myApp.controller('AccountsCtrl', ['$scope', 'LoginService', 'AccountsService', f
         }
     })();
 
+    $scope.validateMail = function (field) {
+        validate(field);
+    }
+
+    function validate(field) {
+        var resultText = undefined;
+        var emails = undefined;
+        if (field == 'from') {
+            resultText = $("#resultFrom");
+            emails = $scope.cuenta.sourceMail;
+        } else {
+            resultText = field == 'to' ? $("#result") : $("#resultCC");
+            emails = field == 'to' ? $scope.cuenta.deliveryMail.split(',') : $scope.cuenta.ccMail.split(',');
+        }
+        resultText.text("");
+
+        if (validateEmail(emails)) {
+            resultText.text("correo valido");
+            resultText.css("color", "green");
+        } else {
+            resultText.text("correo invalido");
+            resultText.css("color", "red");
+        }
+        return false;
+    }
+
+    function validateEmail(arrEmail) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var valid = true;
+        if (arrEmail instanceof Array) {
+            arrEmail.forEach(email => {
+                if (!re.test(email.trim())) valid = false;
+            });
+        } else {
+            valid = re.test(arrEmail.trim());
+        }
+
+        return valid;
+    }
+
     $scope.registro = function (data) { 
         if (data) {//editar
             $scope.cuenta = data;
