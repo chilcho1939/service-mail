7f23bd317d526df823d684c2cc90b07645143414
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2019-03-12 18:24:21 -0600

    se quita perfil para logueo de desarrollo

--- configs/log4js.js
+++ configs/log4js.js
@@ -1,53 +1,31 @@
 const log4js = require('log4js');
 var logger = log4js.getLogger();
 //en los appenders se pueden agregar mas de un log destino aparte de app
-if (process.env.NODE_ENV == 'development') {
-    log4js.configure({
-        appenders: {
-            out: {
-                type: 'stdout'
-            },
-            app: {
-                type: 'file',
-                filename: __dirname + '/logs/application.log',
-                maxLogSize: 20000000,
-                backups: 3,
-                compress: true
-            }
+
+log4js.configure({
+    appenders: {
+        out: {
+            type: 'stdout'
         },
-        categories: {
-            default: {
-                appenders: ['out'],
-                level: 'info'
-            },
-            error: {
-                appenders: ['out', 'app'],
-                level: 'error'
-            }
+        app: {
+            type: 'file',
+            filename: __dirname + '/logs/application.log',
+            maxLogSize: 20000000,
+            backups: 3,
+            compress: true
         }
-    });
-} else { 
-    log4js.configure({
-        appenders: {
-            out: {
-                type: 'stdout'
-            },
-            app: {
-                type: 'file',
-                filename: __dirname + '/logs/application.log',
-                maxLogSize: 20000000,
-                backups: 3,
-                compress: true
-            }
+    },
+    categories: {
+        default: {
+            appenders: ['out'],
+            level: 'info'
         },
-        categories: {
-            default: {
-                appenders: ['app'],
-                level: 'error'
-            }
+        error: {
+            appenders: ['out', 'app'],
+            level: 'error'
         }
-    });
-}
+    }
+});
 
 
 module.exports = logger;
