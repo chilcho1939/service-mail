ffb650a6c159ddd98309ac746301ec16111942d1
Gildardo Ortiz Mercado <gildardo.ortiz@seguritech.com>
2019-03-13 13:05:38 -0600

    se elimina informacion de la pagina de activacion

--- public/app/components/activation/ActivationCtrl.js
+++ public/app/components/activation/ActivationCtrl.js
@@ -1,12 +1,3 @@
 myApp.controller('ActivationCtrl', ['$scope','$stateParams','ActivationService', function ($scope,$stateParams,  ActivationService) {
-    ActivationService.activateAccount($stateParams.token).then(data => {
-        if (data) {
-            $.notify("Cuenta activada exitosamente", 'success', { position: "top right" });
-            $scope.texto = "Cuenta activada exitosamente."
-        } else { 
-            $.notify("El token expiró o la cuenta ya ha sido activada", 'warning', {
-                position: "top right"
-            });
-        }
-    });
+    ActivationService.activateAccount($stateParams.token);
 }])
